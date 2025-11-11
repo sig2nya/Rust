dca7b1e4c1739552bb5dc0e4424cb6926c3d3221
ȫ¹νÄ <starwalk7058@naver.com>
2025-11-11 16:54:48 +0900

    unsafe

---
+++ usafe/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let mut x: i32 = 42;
+    let r1 = &x as *const i32;
+    let 32 = &mut x as *mut i32;
+
+    unsafe {
+        println!("r1 : {}, r2 : {}", *r1, *r2);
+        *r2 = 99;
+        println!("x = {}", x);
+    }
+}
